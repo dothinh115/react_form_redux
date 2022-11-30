705f06d4df819622b7b35f3ac7e89f05ee6d6e22
dothinh115 <dothinh115@gmail.com>
2022-11-30 13:37:15 +0700

    update

--- src/App.js
+++ src/App.js
@@ -3,14 +3,16 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import {BrowserRouter, Routes, Route, Navigate} from 'react-router-dom';
 import Main from './components/Main';
 import List from './components/List';
+import Search from './components/Search';
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Main />}>
-          <Route path="/edit/:userID" element={<List />} />
           <Route index element={<List />} />
+          <Route path="/edit/:userID" element={<List />} />
+          <Route path="/search/:keyWords" element={<List />} />
           <Route path='*' element={<Navigate to="/" />} />
         </Route>
       </Routes>
--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -1,16 +1,32 @@
 import React, { Component } from 'react'
 import { connect } from 'react-redux'
 import { searchAction } from '../redux/actions/userActions';
-import withRouter from '../customFunction/myCustom';
+import customFunc from '../customFunction/myCustom';
 
 export class Search extends Component {
+    constructor(props) {
+      super(props)
+    
+      this.state = {
+         value: ""
+      }
+    }
+
     inputChangeHandle = e => {
+        let {value} = e.target;
+        value = value.trim().toLowerCase();
+        this.setState({
+            value
+        });
+    }
+
+    submitHandle = e => {
+        e.preventDefault();
         const {params, navigate} = this.props;
         if(params.userID) {
             navigate("/");
         }
-        let {value} = e.target;
-        value = value.trim().toLowerCase();
+        const {value} = this.state;
         const action = searchAction(value);
         this.props.dispatch(action);
     }
@@ -20,7 +36,18 @@ export class Search extends Component {
             <h1>
                 Tìm kiếm
             </h1>
-            <input type="text" className="form-control" placeholder="Nhập họ và tên!" onChange={this.inputChangeHandle} />
+            <form onSubmit={this.submitHandle}>
+                <div className="row">
+                    <div className="col-11">
+                        <input type="text" className="form-control" placeholder="Nhập họ và tên!" onChange={this.inputChangeHandle} />
+                    </div>
+                    <div className="col-1">
+                        <button type="submit" className="btn btn-primary">
+                            Tìm
+                        </button>
+                    </div>
+                </div>
+            </form>
         </div>
         )
     }
@@ -29,4 +56,4 @@ export class Search extends Component {
 const mapStateToProps = (state) => ({})
 
 
-export default connect(mapStateToProps)(withRouter(Search))
+export default connect(mapStateToProps)(customFunc(Search))
